7926102590c9500bf010639eb4d0a1a4f87a10fa
Damon Self <damongself@gmail.com>
2019-11-26 16:17:27 -0700

    Removes unused images and adds new assets Removes the mushroom and phaser-es6-webpack images because they were not being used any longer. Adds a new image named All.png under assets/images and also adds a new tileset called OverworldMountains that is based off of All.png.

---
+++ assets/tilesets/OverworldMountains.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="OverworldMountains" tilewidth="32" tileheight="32" tilecount="224" columns="16">
+ <image source="../../../../../Downloads/Pack02/32x32/All.png" width="512" height="448"/>
+</tileset>
